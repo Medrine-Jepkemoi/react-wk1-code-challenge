8ae27a55bb4064317c412207cc4b71597a8ba962
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-04-24 11:56:11 +0300

    All Transactions Table Rendered

--- src/components/transactionstbl.js
+++ src/components/transactionstbl.js
@@ -8,8 +8,33 @@ function TransactionTable() {
   useEffect(() => {
     fetch("http://localhost:3000/transactions")
       .then((res) => res.json())
-      .then((transactions) => console.log(transactions));
+      .then((transactions) => setTransactions(transactions));
   }, []);
+
+  return (
+    <div className="App">
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>DATE</th>
+          <th>DESCRIPTION</th>
+          <th>CATEGORY</th>
+          <th>AMOUNT</th>
+        </tr>
+        {transactions.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.id}</td>
+              <td>{val.date}</td>
+              <td>{val.description}</td>
+              <td>{val.category}</td>
+              <td>{val.amount}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
 }
 
 export default TransactionTable;
